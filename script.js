581151ed8afbe9f6744801c8ecfd9c1bf2493bff
sibasish005 <sibasishchakraborty000@gmail.com>
2025-06-13 03:37:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let  tablinks =  document.getElementsByClassName("tab-links")
+let  tabContents =  document.getElementsByClassName("tab-contents")
+function opentab(tabName){
+    for( tablink of tablinks){
+        tablink.classList.remove("active-link")
+    }
+    for( tabContent of tabContents){
+        tabContent.classList.remove("active-tab")
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabName).classList.add("active-tab")
+}
+
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openMenu(){
+    sidemenu.style.right = "0";
+}
+
+function closeMenu(){
+    sidemenu.style.right = "-200px";
+}
